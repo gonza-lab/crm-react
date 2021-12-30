c4962a3e985f36bf096e702e60373779625c1340
Gonzalo Flores <gonzalo.flores@estoes.me>
2021-12-30 16:18:05 -0300

    add page to show invoices

--- src/app/invoices/index.tsx
+++ src/app/invoices/index.tsx
@@ -1,10 +1,34 @@
-import { Typography } from '@mui/material';
+import { Box, Button, Typography } from '@mui/material';
+import AddIcon from '@mui/icons-material/Add';
 
 const Invoices = () => {
   return (
-    <div>
-      <Typography>Invoices works!</Typography>
-    </div>
+    <Box
+      component="main"
+      sx={{
+        flexGrow: 1,
+        px: 3,
+        py: 8,
+      }}
+    >
+      <Box
+        sx={{
+          display: 'flex',
+          flexDirection: { xs: 'column', md: 'row' },
+          justifyContent: { md: 'space-between' },
+          alignItems: { md: 'center' },
+        }}
+      >
+        <Typography sx={{ mb: 3 }} variant="h4">
+          Recibos
+        </Typography>
+        <Box>
+          <Button variant="contained" startIcon={<AddIcon />}>
+            Nuevo
+          </Button>
+        </Box>
+      </Box>
+    </Box>
   );
 };
 
