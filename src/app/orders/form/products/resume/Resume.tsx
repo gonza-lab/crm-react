3c37418d3fc106acc07a519268186eb5ae2f0a78
Gonzalo Flores <gonzalo.flores@estoes.me>
2022-02-12 13:58:14 -0300

    add message of no products in summary card

--- src/app/orders/form/products/resume/Resume.tsx
+++ src/app/orders/form/products/resume/Resume.tsx
@@ -1,9 +1,11 @@
 import { FunctionComponent } from 'react';
 import { Box, Card, Typography } from '@mui/material';
-import ProductDB from '../../../../../interfaces/ProductDB';
+import ProductionQuantityLimitsIcon from '@mui/icons-material/ProductionQuantityLimits';
 
+import ProductDB from '../../../../../interfaces/ProductDB';
 import { OrderedProduct, OrderedProducts } from '../Products';
 import FormProductsSummaryDetail from './product/Product';
+import CenterAbsolute from '../../../../shared/center-absolute/CenterAbsolute';
 
 const FormProductsSummary: FunctionComponent<{
   products: OrderedProducts;
@@ -15,19 +17,32 @@ const FormProductsSummary: FunctionComponent<{
   };
 
   return (
-    <Card sx={{ p: 3, height: '100%' }}>
+    <Card sx={{ p: 3, height: '100%', position: 'relative' }}>
       <Typography variant="h6" sx={{ mb: 3 }}>
         Resumen
       </Typography>
       <Box sx={{ display: 'flex', flexDirection: 'column', gap: 1 }}>
-        {Object.values(products).map((value) => (
-          <FormProductsSummaryDetail
-            {...value}
-            key={value.product.id}
-            onChangeValue={handleChangeQuantity}
-            onDelete={onDeleteProduct}
-          />
-        ))}
+        {Object.values(products).length ? (
+          Object.values(products).map((value) => (
+            <FormProductsSummaryDetail
+              {...value}
+              key={value.product.id}
+              onChangeValue={handleChangeQuantity}
+              onDelete={onDeleteProduct}
+            />
+          ))
+        ) : (
+          <CenterAbsolute>
+            <Box>
+              <ProductionQuantityLimitsIcon
+                sx={{ margin: '0 auto', display: 'block' }}
+              />
+              <Typography variant="body2" color="textSecondary" mt={2}>
+                No hay productos seleccionados
+              </Typography>
+            </Box>
+          </CenterAbsolute>
+        )}
       </Box>
     </Card>
   );
