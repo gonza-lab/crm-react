16e1b4e0a77325443776407f682e8691896ca83b
Gonzalo Flores <gonzalo.flores@estoes.me>
2022-02-15 09:27:54 -0300

    extract logic of submit in index of add

--- src/app/orders/add/index.tsx
+++ src/app/orders/add/index.tsx
@@ -1,16 +1,53 @@
 import { useEffect } from 'react';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
+import { createOrder } from '../../../state/orders/reducer';
 import { readAllUsers } from '../../../state/users/slice';
-import OrderForm from '../form/Form';
+import OrderForm, { OrderFormOrder } from '../form/Form';
+import { AppDispatch, RootState } from '../../../state/store';
+import { useNavigate } from 'react-router-dom';
+import toast from 'react-hot-toast';
+import { OrderState, OrderStatus } from '../../../state/orders/slice';
 
 const OrderAddIndex = () => {
-  const dispatch = useDispatch();
+  const dispatch = useDispatch<AppDispatch>();
+  const navigate = useNavigate();
+  const { status } = useSelector<RootState, OrderState>(
+    (state) => state.orders
+  );
 
   useEffect(() => {
     dispatch(readAllUsers());
   }, []);
 
-  return <OrderForm />;
+  const handleSubmit = ({ user, products }: OrderFormOrder) => {
+    dispatch(
+      createOrder({
+        userId: user.id,
+        products: Object.values(products).map((value) => ({
+          id: value.product.id,
+          quantity: value.quantity,
+        })),
+        status: 1,
+      })
+    )
+      .unwrap()
+      .then(() => {
+        navigate('/pedidos');
+        toast.success('Se ha creado la orden con éxito.');
+      })
+      .catch(() => {
+        toast.error(
+          'Ha ocurrido un error al crear la orden. Porfavor, intentelo nuevamente.'
+        );
+      });
+  };
+
+  return (
+    <OrderForm
+      onSubmit={handleSubmit}
+      loading={status === OrderStatus.creatingOrder}
+    />
+  );
 };
 
 export default OrderAddIndex;
--- src/app/orders/form/Form.tsx
+++ src/app/orders/form/Form.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { FunctionComponent, useState } from 'react';
 import { Box, Container, Typography } from '@mui/material';
 import SaveIcon from '@mui/icons-material/Save';
 import { LoadingButton } from '@mui/lab';
@@ -7,47 +7,33 @@ import FormCustomer from './customer/Customer';
 import LinkBack from '../../shared/link-back/LinkBack';
 import FormProducts, { OrderedProducts } from './products/Products';
 import UserDB from '../../../interfaces/UserDB';
-import { useDispatch, useSelector } from 'react-redux';
-import { createOrder } from '../../../state/orders/reducer';
-import { RootState } from '../../../state/store';
-import { OrderState, OrderStatus } from '../../../state/orders/slice';
-import { AppDispatch } from '../../../state/store';
-import { useNavigate } from 'react-router-dom';
-import toast from 'react-hot-toast';
+import OrderDB from '../../../interfaces/OrderDB';
 
-const OrderForm = () => {
-  const dispatch = useDispatch<AppDispatch>();
-  const [user, setUser] = useState<UserDB | null>();
-  const [products, setProducts] = useState<OrderedProducts>({});
-  const [submited, setSubmited] = useState(false);
-  const { status } = useSelector<RootState, OrderState>(
-    (state) => state.orders
+export interface OrderFormOrder {
+  user: UserDB;
+  products: OrderedProducts;
+}
+
+const OrderForm: FunctionComponent<{
+  order?: OrderDB;
+  loading?: boolean;
+  onSubmit: (order: OrderFormOrder) => void;
+}> = ({ order, onSubmit, loading }) => {
+  const [user, setUser] = useState<UserDB | null>(order ? order.user : null);
+  const [products, setProducts] = useState<OrderedProducts>(
+    order
+      ? order.products.map((product) => ({
+          quantity: product.quantity,
+          product: product.product,
+        }))
+      : {}
   );
-  const navigate = useNavigate();
+  const [submited, setSubmited] = useState(false);
 
   const handleSubmit = () => {
     setSubmited(true);
     if (user && Object.values(products).length) {
-      dispatch(
-        createOrder({
-          userId: user.id,
-          products: Object.values(products).map((value) => ({
-            id: value.product.id,
-            quantity: value.quantity,
-          })),
-          status: 1,
-        })
-      )
-        .unwrap()
-        .then(() => {
-          navigate('/pedidos');
-          toast.success('Se ha creado la orden con éxito.');
-        })
-        .catch(() => {
-          toast.error(
-            'Ha ocurrido un error al crear la orden. Porfavor, intentelo nuevamente.'
-          );
-        });
+      onSubmit({ user, products });
     }
   };
 
@@ -79,7 +65,7 @@ const OrderForm = () => {
           variant="contained"
           startIcon={<SaveIcon />}
           onClick={handleSubmit}
-          loading={OrderStatus.creatingOrder === status}
+          loading={loading}
         >
           Cargar Orden
         </LoadingButton>
